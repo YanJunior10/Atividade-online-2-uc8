c3aed5130ae5b8f6ea050bf963d78824d98d9470
YanJunior10 <yan.souza@dentalspeed.com>
2022-04-06 22:28:00 -0300

    cadastro de evento

---
+++ Evento.js
@@ -0,0 +1,42 @@
+let = DataDoEvento ("30/04/2022");
+let = DataAtual ("06/04/2022");
+
+if (DataDoEvento > DataAtual) {
+    console.log ("Evento Permitido");
+}
+else {
+    console.log ("Evento Indisponivel");
+}
+    
+
+const Idade = 18
+
+if (Idade > 18) {
+    console.log ("Cadastro realizado informe a lista de particpantes");
+}   
+else {
+    console.log ("Cadastro não permitido pela idade");
+}
+    
+
+var ListaDeParticipantes
+[
+    'João',
+    'Miguel',
+    'Fernando',
+    'Luiz',
+    'Augusto',
+    'Amanda',
+    'Luana',
+    'Renata',
+]
+
+if (ListaDeParticipantes.length < 101) {
+   console.log ("Cadastro permitido!");
+}
+else { 
+    console.log ("Limite de participantes excedido!");
+}
+
+
+
